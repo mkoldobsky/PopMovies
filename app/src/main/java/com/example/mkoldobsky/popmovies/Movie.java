931e279595f33d7f4dc9f8e431c247e8b948c7e3
Mariano Koldobsky <mariano.koldobsky@mercadolibre.com>
2015-09-04 11:48:25 -0300

    Detail with toolbar working

--- app/src/main/java/com/example/mkoldobsky/popmovies/Movie.java
+++ app/src/main/java/com/example/mkoldobsky/popmovies/Movie.java
@@ -1,20 +1,40 @@
 package com.example.mkoldobsky.popmovies;
 
+import android.os.Bundle;
+
 /**
  * Created by mkoldobsky on 2/9/15.
  */
 public class Movie {
-    String title;
-    String posterPath;
-    private String plotSinapsys;
+    private String title;
+    private String posterPath;
+    private String plotSynopsis;
     private Double voteAverage;
+    private String releaseDate;
 
-    public Movie(String title, String posterPath, String plot, Double vote){
+    final static String TITTLE_KEY = "title";
+    final static String PATH_KEY = "path";
+    final static String PLOT_KEY = "plot";
+    final static String VOTE_KEY = "vote";
+    final static String DATE_KEY = "date";
+
+    public Movie(String title, String posterPath, String plot, Double vote, String date){
 
         this.title = title;
         this.posterPath = posterPath;
-        plotSinapsys = plot;
-        voteAverage = vote;
+        this.plotSynopsis = plot;
+        this.voteAverage = vote;
+        this.releaseDate = date;
+    }
+
+    public Movie (Bundle bundle){
+        if (bundle != null) {
+            this.title = bundle.getString(TITTLE_KEY);
+            this.posterPath = bundle.getString(PATH_KEY);
+            this.plotSynopsis = bundle.getString(PLOT_KEY);
+            this.voteAverage = bundle.getDouble(VOTE_KEY);
+            this.releaseDate = bundle.getString(DATE_KEY);
+        }
     }
 
     public String getTitle() {
@@ -23,5 +43,43 @@ public class Movie {
 
     public String getPosterPath() {
         return posterPath;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+
+    public void setPlotSynopsis(String plotSynopsis) {
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public Bundle getBundle(){
+        Bundle bundle = new Bundle();
+        bundle.putString(TITTLE_KEY, this.title);
+        bundle.putString(PATH_KEY, this.posterPath);
+        bundle.putString(PLOT_KEY, this.plotSynopsis);
+        bundle.putDouble(VOTE_KEY, this.voteAverage);
+        bundle.putString(DATE_KEY, this.releaseDate);
+        return bundle;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
     }
 }
