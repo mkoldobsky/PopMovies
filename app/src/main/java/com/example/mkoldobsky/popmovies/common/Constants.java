d10703b0286e95c75d409ed6ba371409f3a73e57
Mariano Koldobsky <mariano.koldobsky@mercadolibre.com>
2015-09-08 10:12:18 -0300

    Of

--- app/src/main/java/com/example/mkoldobsky/popmovies/common/Constants.java
+++ app/src/main/java/com/example/mkoldobsky/popmovies/common/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
       more instruction on
       https://docs.google.com/document/d/1ZlN1fUsCSKuInLECcJkslIqvpKlP7jWL2TP9m6UiA6I/pub?embedded=true
       */
-    final public static String API_KEY = "2c6f7527e6f0169629e828e31f25db75"; // need to be obfuscated
+    final public static String API_KEY = "xxxx"; // need to be obfuscated
 
 
     final public static String MOST_POPULAR_SORT_ORDER = "most_popular";
